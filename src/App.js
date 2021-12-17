3c0e76ce56d30cd6efdd196275aa8686062f1d1a
Jonathan Day <jonday@jonday.dev>
2021-12-17 16:06:06 -0600

    test some stuff

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './components/Nav/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      Is this working?
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Navbar.js
@@ -0,0 +1,16 @@
+import styles from './Navbar.module.css';
+import logo from '../../assets/logo.png';
+
+const Navbar = props => {
+    return <nav className={styles.navbar}>
+        <img src={logo} alt="logo" className={styles.logo} />
+        <section className={styles.links}>
+            <a href="http://localhost:3000" className={styles['link']}>Home</a>
+            <a href="http://localhost:3000" className={styles['link']}>About Us</a>
+            <a href="http://localhost:3000" className={styles['link']}>2022 Virtual Portrait Contest</a>
+            <a href="http://localhost:3000" className={styles['link']}>Online Art Demos</a>
+        </section>
+    </nav>
+}
+
+export default Navbar;
